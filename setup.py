880bf14f48bbee1a26cd32385e127480ed42036f
nbtk <horie@bitmeister.jp>
2024-04-24 10:29:22 +0900

    Preparation for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='momonga',
-    version='0.0.1',
+    version='0.0.2',
     description='Python Route B Library: A Comunicator for Low-voltage Smart Electric Energy Meters',
     long_description=long_description,
     long_description_content_type='text/markdown',
